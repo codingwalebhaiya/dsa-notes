6b13adf9db5e65c4d4236097372dcb62f197a3bc
SATYAM KUMAR PANDEY <116620645+codingwalebhaiya@users.noreply.github.com>
2025-08-25 09:37:26 +0530

    Segregate 0s and 1s

--- arrays/array.cpp
+++ arrays/array.cpp
@@ -1,3 +1,52 @@
+# Problem: Segregate 0s and 1s
+
+**Platform**: GFG  
+**Difficulty**: easy  
+**Link**: https://www.geeksforgeeks.org/problems/segregate-0s-and-1s5106/1
+
+---
+
+## Problem Statement
+Given an array arr consisting of only 0's and 1's in random order. Modify the array in-place to segregate 0s onto the left side and 1s onto the right side of the array.
+
+Examples :
+
+Input: arr[] = [0, 0, 1, 1, 0]
+Output: [0, 0, 0, 1, 1]
+Explanation:  After segregation, all the 0's are on the left and 1's are on the right. Modified array will be [0, 0, 0, 1, 1].
+
+ 
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        
+        int n= arr.size();
+        int start=0, end=n-1;
+        
+        while(start<end){
+            if(arr[start] ==0){
+                start++; 
+            }
+           
+            else {
+                if(arr[end] ==0){
+                      swap(arr[start], arr[end]);
+                start++, end--;
+                }
+              
+                else {
+                    end--;
+                }
+                
+            } 
+            
+        }
+        
+    }
+};
+
+
+
 # Problem: 167. Two Sum II - Input Array Is Sorted
 
 **Platform**: LeetCode  
@@ -218,4 +267,5 @@ public:
 
 
   
+
 
